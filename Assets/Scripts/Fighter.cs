2469c0433bfc3cf194d24c9c358532abf88e2949
Al4892 <vitekaren89@gmail.com>
2025-07-04 19:57:54 -0600

    FinalCommit

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Fighter : MonoBehaviour
+{
+    [SerializeField]
+    private string _name;
+    public string Name => _name;
+    [SerializeField]
+    private Health _health;
+    [SerializeField]
+    private Animator _characterAnimator;
+    [SerializeField]
+    private Attacks _attacks;
+    public Health Health => _health;
+    public Attacks Attacks => _attacks;
+    public Animator CharacterAnimator => _characterAnimator;
+    [SerializeField]
+    private UnityEvent _onFighterInitialized;
+    [SerializeField]
+    private string _winAnimationName = "Win";
+    public string WinAnimationName => _winAnimationName;
+    [SerializeField]
+    private string _winSoundName = "WinSound";
+    public string WinSoundName => _winSoundName;
+    public void InitializeFighter()
+    {
+        _onFighterInitialized?.Invoke();
+    }
+}
